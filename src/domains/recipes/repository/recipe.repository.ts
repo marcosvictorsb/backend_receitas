83b48fcdc45c5e1eca1236592310c364de01a17c
marcos <marcosvictorsb@gmail.com>
2026-05-13 20:21:42 -0300

    refactor: corrigindo sintaxe

--- src/domains/recipes/repository/recipe.repository.ts
+++ src/domains/recipes/repository/recipe.repository.ts
@@ -61,7 +61,7 @@ export class RecipeRepository implements IRecipeRepository {
         ...(params.search
           ? {
               name: {
-                [Op.iLike]: `%${params.search}%`
+                [Op.like]: `%${params.search}%`
               }
             }
           : {})
