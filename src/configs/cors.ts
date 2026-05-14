f0cd0bf08c5ee4d83def93fd5ea1791f50dbd4f2
marcos <marcosvictorsb@gmail.com>
2026-05-14 14:54:33 -0300

    chore: atualizando o cors e seeds para ignorar os duplicados

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -1,6 +1,10 @@
 import cors from 'cors';
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'http://localhost:8080'
+];
 
 export const corsOptions: cors.CorsOptions = {
   origin: (origin, callback) => {
--- src/infra/database/seeders/20260514105141-categories-initial.js
+++ src/infra/database/seeders/20260514105141-categories-initial.js
@@ -3,21 +3,25 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('categorias', [
-      { id: 1, nome: 'Bolos e tortas doces' },
-      { id: 2, nome: 'Carnes' },
-      { id: 3, nome: 'Aves' },
-      { id: 4, nome: 'Peixes e frutos do mar' },
-      { id: 5, nome: 'Saladas, molhos e acompanhamentos' },
-      { id: 6, nome: 'Sopas' },
-      { id: 7, nome: 'Massas' },
-      { id: 8, nome: 'Bebidas' },
-      { id: 9, nome: 'Doces e sobremesas' },
-      { id: 10, nome: 'Lanches' },
-      { id: 11, nome: 'Prato Único' },
-      { id: 12, nome: 'Light' },
-      { id: 13, nome: 'Alimentação Saudável' }
-    ]);
+    await queryInterface.bulkInsert(
+      'categorias',
+      [
+        { id: 1, nome: 'Bolos e tortas doces' },
+        { id: 2, nome: 'Carnes' },
+        { id: 3, nome: 'Aves' },
+        { id: 4, nome: 'Peixes e frutos do mar' },
+        { id: 5, nome: 'Saladas, molhos e acompanhamentos' },
+        { id: 6, nome: 'Sopas' },
+        { id: 7, nome: 'Massas' },
+        { id: 8, nome: 'Bebidas' },
+        { id: 9, nome: 'Doces e sobremesas' },
+        { id: 10, nome: 'Lanches' },
+        { id: 11, nome: 'Prato Único' },
+        { id: 12, nome: 'Light' },
+        { id: 13, nome: 'Alimentação Saudável' }
+      ],
+      { ignoreDuplicates: true }
+    );
   },
 
   async down(queryInterface, Sequelize) {
